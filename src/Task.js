6160bc2135b281e512de182943465b41b00afa49
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-12-15 06:10:14 +0800

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
-const studentName = "YOURNAME"; // TODO: replace this
+const url = "https://photo-gallery-app-netlify-deployment.netlify.app/"; // TODO: replace this
+const studentName = "NI KADEK YULIA DEWI"; // TODO: replace this
 const studentId = "FE2772427"; // TODO: replace this
 
 export { url, studentName, studentId };
